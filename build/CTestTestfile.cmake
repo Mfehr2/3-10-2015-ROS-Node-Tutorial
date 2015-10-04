fa0d28e628f07e85292993cfcdda7988e2cda948
Chris <chris@Notebook.(none)>
2015-10-04 12:14:57 -0400

    Created Catkin Workspace

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/chris/cprt/ROS Node Tutorial/src
+# Build directory: /home/chris/cprt/ROS Node Tutorial/build
+# 
+# This file includes the relevent testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CATKIN_TEST_RESULTS_DIR="/home/chris/cprt/ROS Node Tutorial/build/test_results"
+export CMAKE_PREFIX_PATH="/home/chris/cprt/ROS Node Tutorial/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/chris/cprt/ROS Node Tutorial/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/chris/cprt/ROS Node Tutorial/devel/lib:$LD_LIBRARY_PATH"
+export PATH="/home/chris/cprt/ROS Node Tutorial/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/chris/cprt/ROS Node Tutorial/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PWD="/home/chris/cprt/ROS Node Tutorial/build"
+export PYTHONPATH="/home/chris/cprt/ROS Node Tutorial/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/chris/cprt/ROS Node Tutorial/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/chris/cprt/ROS Node Tutorial/src:$ROS_PACKAGE_PATH"
+export ROS_TEST_RESULTS_DIR="/home/chris/cprt/ROS Node Tutorial/build/test_results"
